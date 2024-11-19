3652e4981a38883e8582c561d3d49a6f3a243aac
Linus Ngwee <linusngwee@yahoo.com>
2024-11-19 09:38:14 +0100

    Reverse2

--- dataconnect-generated/js/default-connector/index.cjs.js
+++
@@ -1,9 +0,0 @@
-const { getDataConnect, validateArgs } = require('firebase/data-connect');
-
-const connectorConfig = {
-  connector: 'default',
-  service: 'Make-TON-Telegram-Mini-App-1-master',
-  location: 'us-central1'
-};
-exports.connectorConfig = connectorConfig;
-
